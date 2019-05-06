5603c9ba2dafe47af0176bfc3c6c6705ccf2c4c4
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-05-06 11:28:48 -0600

    chore: Initial state

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { HttpStatus } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', done => {
+    request(app.getHttpServer())
+      .get('/')
+      .expect(HttpStatus.OK)
+      .expect('Hello World!')
+      .end(done);
+  });
+});
