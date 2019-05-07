84b145937e556066b74508f39ffd892a39d6abaa
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-05-06 22:14:17 -0600

    chore: Load fixture before end-to-end tests
    Install typeorm-fixtures-cli to fixtures into database

---
+++ test/fixtures/user.processor.ts
@@ -0,0 +1,11 @@
+import * as bcrypt from 'bcryptjs';
+import { IProcessor } from 'typeorm-fixtures-cli';
+
+import { User } from '../../src/user/user.entity';
+
+export default class UserProcessor implements IProcessor<User> {
+  postProcess(name: string, object: User) {
+    const salt = bcrypt.genSaltSync();
+    object.password = bcrypt.hashSync(object.password, salt);
+  }
+}
