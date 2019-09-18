36e7ff351be556f943909e43c5f2e20f9f150cac
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-09-18 13:52:45 -0600

    feat(auth): Implement session guard
    Install cookie-parser and express-session.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 import * as passport from 'passport';
 
 import { AppModule } from './app.module';
@@ -7,6 +9,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.use(cookieParser(process.env.APP_SECRET));
+  app.use(
+    session({
+      secret: process.env.APP_SECRET as string,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   app.use(passport.initialize());
   app.use(passport.session());
   await app.listen(process.env.PORT || 3000);
