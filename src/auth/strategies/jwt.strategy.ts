0bc7d1b76ccb73c7c979725b9c00bde924900679
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-23 20:18:58 -0600

    fix(auth): Do not ignore token expiration

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -12,6 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.APP_SECRET,
+      ignoreExpiration: false,
       passReqToCallback: false,
     });
   }
