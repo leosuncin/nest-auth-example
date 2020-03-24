87faac72510cdfc8329cf05e1ccc10b8a80346d8
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-23 18:19:09 -0600

    fix(auth): Import passport module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './strategies/local.strategy';
-import { JwtStrategy } from './strategies/jwt.strategy';
 import { SessionSerializer } from './session.serializer';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
     UserModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.APP_SECRET,
       signOptions: {
