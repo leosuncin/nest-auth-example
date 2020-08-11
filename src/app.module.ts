b6b4da7ab5e75a99c056d47e30cb2fc3bf1028f1
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-08-10 18:34:28 -0600

    feat(todo): Add todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
