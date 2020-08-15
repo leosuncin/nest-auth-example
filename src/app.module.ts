039be4a242d8263d14cbc64807207a4504977306
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-08-15 13:08:18 -0600

    feat: Setup mailer module
    Use SMTP connection.
    Use EJS template engine.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BullModule } from '@nestjs/bull';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,6 +17,22 @@ import { EMAIL_QUEUE_NAME } from './constants';
     BullModule.registerQueue({
       name: EMAIL_QUEUE_NAME,
       redis: process.env.REDIS_URL,
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST || 'localhost',
+        port: parseInt(process.env.SMTP_PORT, 10) || 1025,
+        secure: process.env.SMTP_SECURE === 'true',
+        ignoreTLS: process.env.SMTP_SECURE !== 'false',
+        auth: {
+          user: process.env.SMTP_AUTH_USER || 'username',
+          pass: process.env.SMTP_AUTH_PASS || 'password',
+        },
+      },
+      template: {
+        dir: process.cwd() + '/templates/',
+        adapter: new EjsAdapter(),
+      },
     }),
     UserModule,
     AuthModule,
