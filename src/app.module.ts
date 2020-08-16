d775eba599a4a0624db98b0cf9c3d9f991652ad0
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-08-16 14:31:27 -0600

    feat(mail): Setup Mailer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,7 +10,28 @@ import { AuthModule } from './auth/auth.module';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, TodoModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAILGUN_SMTP_SERVER,
+        port: parseInt(process.env.MAILGUN_SMTP_PORT, 10),
+        secure: process.env.SMTP_SECURE === 'true',
+        ignoreTLS: process.env.SMTP_SECURE !== 'true',
+        auth: {
+          user: process.env.MAILGUN_SMTP_LOGIN,
+          pass: process.env.MAILGUN_SMTP_PASSWORD,
+        },
+      },
+      template: {
+        dir: process.cwd() + '/templates/',
+        adapter: new EjsAdapter(),
+      },
+    }),
+    UserModule,
+    AuthModule,
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
