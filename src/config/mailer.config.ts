fff47b5a28ce866cffcdeb8c844af39354d18c53
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-08-17 12:38:04 -0600

    feat(mail): Use Mailgun transporter in production

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -1,8 +1,10 @@
 import { registerAs } from '@nestjs/config';
 import { MailerOptions } from '@nestjs-modules/mailer';
 import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import * as mailgunTransport from 'nodemailer-mailgun-transport';
 
 export default registerAs<() => MailerOptions>('mailer', () => {
+  const prod = process.env.NODE_ENV === 'production';
   const host = process.env.MAILGUN_SMTP_SERVER;
   const port = parseInt(process.env.MAILGUN_SMTP_PORT, 10);
   const secure = ['true', '1', 1].includes(process.env.SMTP_SECURE);
@@ -10,7 +12,13 @@ export default registerAs<() => MailerOptions>('mailer', () => {
   const pass = process.env.MAILGUN_SMTP_PASSWORD;
 
   return {
-    transport: {
+    transport: prod ? mailgunTransport({
+      auth: {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        api_key: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
+      }
+    }) : {
       host,
       port,
       secure,
