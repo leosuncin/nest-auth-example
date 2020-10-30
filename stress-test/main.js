c3201553feb94ce29ee4c1d6c4c46f6573cd16d1
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-10-29 18:12:13 -0600

    test(stress): Add stress test for authentication

--- stress-test/main.js
+++ stress-test/main.js
@@ -1,11 +1,19 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
+import { requestLogin, requestRegister } from './auth.js';
+
 const baseUrl = 'http://localhost:3000';
 
 export const options = {
   vus: 300,
   duration: '30s',
+  discardResponseBodies: true,
+  thresholds: {
+    checks: ['rate>0.9'],
+    'failed login request': ['rate < 0.1'],
+    'failed register request': ['rate < 0.1'],
+  },
 };
 
 export default function () {
@@ -14,5 +22,7 @@ export default function () {
   check(res, {
     'status is OK': res => res.status === 200,
   });
+  requestLogin(baseUrl);
+  requestRegister(baseUrl);
   sleep(1);
 }
