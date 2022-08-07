621eb3e58596371b88b38942a6373b3cbecbbf00
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-08-06 21:53:41 -0600

    test(todo): refactor unit test for TodoController
    Use ts-auto-mock to create the mocks

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,81 +1,31 @@
-import { faker } from '@faker-js/faker';
-import { build, perBuild, sequence } from '@jackfranklin/test-data-bot';
-import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { NotFoundException, ForbiddenException } from '@nestjs/common';
-import { mock, Matcher } from 'jest-mock-extended';
-import type { Repository } from 'typeorm';
+import { Test, type TestingModule } from '@nestjs/testing';
+import { createMock } from 'ts-auto-mock';
 
+import type { User } from '../user/user.entity';
+import type { TodoCreate } from './todo-create.dto';
+import type { TodoUpdate } from './todo-update.dto';
 import { TodoController } from './todo.controller';
+import type { Todo } from './todo.entity';
 import { TodoService } from './todo.service';
-import { Todo } from './todo.entity';
-import { User } from '../user/user.entity';
-
-const userBuilder = build<User>({
-  fields: {
-    id: sequence(),
-    name: perBuild(() => faker.name.findName()),
-    email: perBuild(() => faker.internet.exampleEmail()),
-    createdAt: perBuild(() => new Date()),
-    updatedAt: perBuild(() => new Date()),
-    password: undefined,
-    checkPassword: perBuild(() => jest.fn()),
-    hashPassword: perBuild(() => jest.fn()),
-  },
-  postBuild: u => new User(u),
-});
-const todoBuilder = build<Todo>({
-  fields: {
-    id: sequence(),
-    text: perBuild(() => faker.lorem.sentence()),
-    done: perBuild(() => faker.datatype.boolean()),
-    owner: userBuilder(),
-    createdAt: perBuild(() => new Date()),
-    updatedAt: perBuild(() => new Date()),
-  },
-  postBuild: t => Object.assign(new Todo(), t),
-});
 
 describe('Todo Controller', () => {
   let controller: TodoController;
+  let mockedTodoService: jest.Mocked<TodoService>;
+  const user = createMock<User>({ id: 1 });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
-      providers: [
-        TodoService,
-        {
-          provide: getRepositoryToken(Todo),
-          useFactory() {
-            const todo = todoBuilder({ overrides: { id: 1, owner: 1 } });
-            const repositoryMocked = mock<Repository<Todo>>({
-              create: jest
-                .fn()
-                .mockImplementation(dto => Object.assign(new Todo(), dto)),
-              save: jest
-                .fn()
-                .mockImplementation(entity =>
-                  Promise.resolve(todoBuilder({ overrides: entity })),
-                ),
-              merge: jest.fn().mockImplementation(Object.assign),
-            });
-
-            repositoryMocked.findOne
-              .calledWith(
-                // @ts-expect-error expect a single option
-                new Matcher(options => options.where.id > 0, 'todo.id'),
-              )
-              .mockResolvedValueOnce(todo);
-            repositoryMocked.find.mockResolvedValue([todo]);
-            repositoryMocked.remove.mockResolvedValue(todo);
-
-            return repositoryMocked;
-          },
-        },
-      ],
-    }).compile();
+    })
+      .useMocker(token => {
+        if (Object.is(token, TodoService)) {
+          return createMock<TodoService>();
+        }
+      })
+      .compile();
 
     controller = module.get<TodoController>(TodoController);
+    mockedTodoService = module.get(TodoService);
   });
 
   it('should be defined', () => {
@@ -83,95 +33,68 @@ describe('Todo Controller', () => {
   });
 
   it('should create a new todo', async () => {
-    const newTodo = {
+    const newTodo: TodoCreate = {
       text: 'Make a sandwich',
+      owner: user,
     };
-    const user = userBuilder();
 
-    await expect(
-      controller.createTodo(newTodo as any, user as any),
-    ).resolves.toBeInstanceOf(Todo);
+    mockedTodoService.createTodo.mockResolvedValue(createMock<Todo>(newTodo));
+
+    await expect(controller.createTodo(newTodo, user)).resolves.toBeDefined();
   });
 
   it('should list all todos', async () => {
-    await expect(
-      controller.listTodo(userBuilder() as any),
-    ).resolves.toBeDefined();
+    const todos = await controller.listTodo(user);
+
+    expect(Array.isArray(todos)).toBe(true);
   });
 
   it('should get one todo', async () => {
-    const user = userBuilder({ overrides: { id: 1 } });
-    await expect(controller.getTodo(1, user as any)).resolves.toBeDefined();
-  });
+    const id = 1;
 
-  it('should fail to get nonexisting todo', async () => {
-    const user = userBuilder();
-    await expect(controller.getTodo(0, user as any)).rejects.toThrow(
-      NotFoundException,
-    );
-  });
+    mockedTodoService.getTodo.mockResolvedValueOnce(createMock<Todo>({ id }));
+    const todo = await controller.getTodo(id, user);
 
-  it("should fail to get another's todo", async () => {
-    const user = userBuilder();
-    await expect(controller.getTodo(1, user as any)).rejects.toThrow(
-      ForbiddenException,
-    );
+    expect(todo).toHaveProperty('id', id);
   });
 
   it('should update one todo', async () => {
-    const user = userBuilder({ overrides: { id: 1 } });
-    const updates = { done: true };
-    await expect(
-      controller.updateTodo(1, updates, user as any),
-    ).resolves.toBeDefined();
-  });
+    const updates: TodoUpdate = {
+      done: true,
+      text: 'Duis do labore enim in irure.',
+    };
 
-  it('should fail to update nonexisting todo', async () => {
-    const user = userBuilder();
-    const updates = { done: true };
-    await expect(
-      controller.updateTodo(0, updates, user as any),
-    ).rejects.toThrow(NotFoundException);
-  });
+    mockedTodoService.updateTodo.mockResolvedValueOnce(
+      createMock<Todo>(updates),
+    );
+    const todo = await controller.updateTodo(1, updates, user);
 
-  it("should fail to update another's todo", async () => {
-    const user = userBuilder();
-    const updates = { done: true };
-    await expect(
-      controller.updateTodo(1, updates, user as any),
-    ).rejects.toThrow(ForbiddenException);
+    expect(todo).toHaveProperty('done', updates.done);
+    expect(todo).toHaveProperty('text', updates.text);
   });
 
   it('should remove one todo', async () => {
-    const user = userBuilder({ overrides: { id: 1 } });
-    await expect(controller.removeTodo(1, user as any)).resolves.toBeDefined();
-  });
-
-  it('should fail to remove nonexisting todo', async () => {
-    const user = userBuilder();
-    await expect(controller.removeTodo(0, user as any)).rejects.toThrow(
-      NotFoundException,
-    );
-  });
-
-  it("should fail to remove another's todo", async () => {
-    const user = userBuilder();
-    await expect(controller.removeTodo(1, user as any)).rejects.toThrow(
-      ForbiddenException,
-    );
+    await expect(controller.removeTodo(1, user)).resolves.toBeDefined();
   });
 
   it('should mark todo as done', async () => {
-    const user = userBuilder({ overrides: { id: 1 } });
-    await expect(
-      controller.markTodoAsDone(1, user as any),
-    ).resolves.toHaveProperty('done', true);
+    mockedTodoService.updateTodo.mockResolvedValueOnce(
+      createMock<Todo>({ done: true }),
+    );
+    const todo = await controller.markTodoAsDone(1, user);
+
+    expect(todo).toHaveProperty('done', true);
   });
 
   it('should mark todo as pending', async () => {
-    const user = userBuilder({ overrides: { id: 1 } });
-    await expect(
-      controller.markTodoAsPending(1, user as any),
-    ).resolves.toHaveProperty('done', false);
+    mockedTodoService.getTodo.mockResolvedValueOnce(
+      createMock<Todo>({ done: true }),
+    );
+    mockedTodoService.updateTodo.mockResolvedValueOnce(
+      createMock<Todo>({ done: false }),
+    );
+    const todo = await controller.markTodoAsPending(1, user);
+
+    expect(todo).toHaveProperty('done', false);
   });
 });
