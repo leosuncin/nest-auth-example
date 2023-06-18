9892ad3b44aeabae7d6224721b62e65b7e327c2f
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2023-06-18 15:14:30 -0600

    fix: extract JWT from cookies

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Strategy, ExtractJwt, JwtFromRequestFunction } from 'passport-jwt';
 
 import { AuthService } from '../auth.service';
 import { User } from '../../user/user.entity';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
+const extractJwtFromCookie: JwtFromRequestFunction = request => {
+  return request.signedCookies['token']!; // eslint-disable-line @typescript-eslint/no-non-null-assertion
+};
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private readonly authService: AuthService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        extractJwtFromCookie,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
       secretOrKey: process.env.APP_SECRET,
       ignoreExpiration: false,
       passReqToCallback: false,
