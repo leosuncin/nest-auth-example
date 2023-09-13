c3dde77e90b4b294c4fe998e00f6f8d90b3ff400
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2023-09-13 17:29:29 -0600

    test: use IntegreSQL connection
    Override the TypeORM module to connect to IntegreSQL
    Run migrations

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,60 @@
-import { HttpStatus } from '@nestjs/common';
+import { IntegreSQLClient } from '@devoxa/integresql-client';
+import { HttpStatus, type INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as request from 'supertest';
 
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { appDataSource as dataSource } from '../src/data-source';
 import { setup } from '../src/setup';
 
+const client = new IntegreSQLClient({
+  url: process.env['INTEGRESQL_URL'] ?? 'http://localhost:5000',
+});
+
 describe('AppController (e2e)', () => {
-  let app;
+  let app: INestApplication;
+  let hash: string;
+
+  beforeAll(async () => {
+    hash = await client.hashFiles([
+      './src/migrations/**/*',
+      './src/**/*.factory.ts',
+      './src/**/*.seeder.ts',
+    ]);
+
+    await client.initializeTemplate(hash, async dbConfig => {
+      dataSource.setOptions({
+        username: dbConfig.username,
+        password: dbConfig.password,
+        database: dbConfig.database,
+        port: dbConfig.port,
+      });
+
+      await dataSource.initialize();
+      await dataSource.runMigrations();
+      await dataSource.destroy();
+    });
+  });
 
   beforeEach(async () => {
+    const dbConfig = await client.getTestDatabase(hash);
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideModule(TypeOrmModule)
+      .useModule(
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          port: dbConfig.port,
+          synchronize: false,
+          autoLoadEntities: true,
+        }),
+      )
+      .compile();
 
     app = setup(moduleFixture.createNestApplication());
 
